9ef5a35792ee5471a4d007a201ed64ae1b663cb3
feiercommon <1311143874@qq.com>
2016-12-13 14:28:28 +0800

    music-app2

--- js/index.js
+++ js/index.js
@@ -1,21 +1,27 @@
-$(function() {
-    function resise(designSize, type) {
-        var type = type || "x";
+$(function(){
+    function resise(designSize,type) {
+        var type= type || "x";
         var width = document.documentElement.clientWidth;
         var height = document.documentElement.clientHeight;
-        if (type == "x") {
-            var scale = width / designSize * 100 + "px";
-        } else if (type == "y") {
-            var scale = height / designSize * 100 + "px";
+        if(type == "x"){
+            var scale = width/designSize*100+"px";
+        }else if(type == "y"){
+            var scale = height/designSize*100+"px";
         }
         document.getElementsByTagName("html")[0].style.fontSize = scale;
     }
-
-    resise(1334, "y");
+    resise(1334,"y");
     var mySwiper = new Swiper ('.swiper-container', {
 
         // 如果需要分页器
-        pagination: '.swiper-pagination'
+        pagination: '.swiper-pagination',
+        onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiper); //隐藏动画元素
+            swiperAnimate(swiper); //初始化完成开始动画
+        },
+        onSlideChangeEnd: function(swiper){
+            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        }
     })
 
-})
+});
