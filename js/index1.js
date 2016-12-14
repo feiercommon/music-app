19a933739923d0d1595a6476149c7689203dc9e5
feiercommon <1311143874@qq.com>
2016-12-14 14:01:32 +0800

    musicapp4

--- js/index1.js
+++ js/index1.js
@@ -11,18 +11,18 @@ $(function(){
         document.getElementsByTagName("html")[0].style.fontSize = scale;
     }
     resise(750,"x");
-    // var mySwiper = new Swiper ('.swiper-container', {
-    //
-    //     // 如果需要分页器
-    //     pagination: '.swiper-pagination',
-    //     onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
-    //         swiperAnimateCache(swiper); //隐藏动画元素
-    //         swiperAnimate(swiper); //初始化完成开始动画
-    //     },
-    //     onSlideChangeEnd: function(swiper){
-    //         swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
-    //     }
-    // })
+    var mySwiper = new Swiper ('.swiper-container', {
+
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
+        // onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+        //     swiperAnimateCache(swiper); //隐藏动画元素
+        //     swiperAnimate(swiper); //初始化完成开始动画
+        // },
+        // onSlideChangeEnd: function(swiper){
+        //     swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        // }
+    })
 
     var navXinwen = $("#navs .navbox .xinwen");
     navXinwen.on("click",function () {
